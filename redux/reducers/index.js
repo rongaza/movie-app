21532658409abb893d5752e1c41b12b36d972807
Ron Gaza <rongaza@gmail.com>
2020-05-13 10:34:17 -0700

    index.js file in reducers had errors in the import statement for favoritesReducer and watchListReducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,12 +1,10 @@
-import favoritesReducer from '../reducers/favoritesReducer';
-import watchListReducer from '../reducers/watchListReducer';
-import movieReducer from '../reducers/movieReducer';
+import favoritesReducer from './favoritesReducer';
+import watchListReducer from './watchListReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
 	favorites: favoritesReducer,
 	watchList: watchListReducer,
-	// movieDetails: movieReducer,
 });
 
 export default rootReducer;
