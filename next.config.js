c29607ee4d5e89b62866816bf7804ebc57a5d99c
Ron Gaza <rongaza@gmail.com>
2020-05-14 21:15:05 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,8 @@ const Dotenv = require('dotenv-webpack');
 
 module.exports = {
 	env: {
-<<<<<<< HEAD
 		MOVIE_API_KEY: process.env.MOVIE_API_KEY,
-=======
 		movieAPIKey: '',
->>>>>>> d149cb1da49f6a9af4f784744b81716e8d17c7f8
 	},
 
 	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
